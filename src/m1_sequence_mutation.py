ad7f580e03ad6eedc9c37e8551b37b1150c5e85d
myersjm <myersjm@rose-hulman.edu>
2017-10-19 14:17:39 -0400

    final commit -Jessica Myers

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS, SEQUENCES and MUTATION  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jessica Myers.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -29,11 +29,34 @@ def run_test_zero_changer():
     print('Test 1:')
     print('  Expected:', expected1)
     print('  Actual:  ', test1)
+    if test1 == expected1:
+        print("PASSED")
 
     # ------------------------------------------------------------------
-    # TODO: 2. Write at least 2 additional tests for the  zero_changer   function.
+    # DONE: 2. Write at least 2 additional tests for the  zero_changer   function.
     #   Try do do some unexpected things like empty lists or an empty tuple.
     # ------------------------------------------------------------------
+    # Test 2:
+    test2 = ([0, 0, 0, 9], [9, 90, 43, 0], [], [0])
+    expected2 = ([1, 2, 3, 9], [9, 90, 43, 4], [], [5])
+    zero_changer(test2)
+    print()
+    print('Test 2:')
+    print('  Expected:', expected2)
+    print('  Actual:  ', test2)
+    if test2 == expected2:
+        print("PASSED")
+
+    # Test 3:
+    test3 = ([], [-3, 20, 303, 9], [0], [5, 0])
+    expected3 = ([], [-3, 20, 303, 9], [1], [5, 2])
+    zero_changer(test3)
+    print()
+    print('Test 3:')
+    print('  Expected:', expected3)
+    print('  Actual:  ', test3)
+    if test3 == expected3:
+        print("PASSED")
 
 
 def zero_changer(tuple_of_lists):
@@ -61,7 +84,7 @@ def zero_changer(tuple_of_lists):
       :type tuple_of_lists: tuple of list[int]
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -69,6 +92,12 @@ def zero_changer(tuple_of_lists):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  10 minutes.
     # ------------------------------------------------------------------
+    number_of_zeros = 0
+    for k in range(len(tuple_of_lists)):
+        for j in range(len(tuple_of_lists[k])):
+            if tuple_of_lists[k][j] == 0:
+                number_of_zeros = number_of_zeros + 1
+                tuple_of_lists[k][j] = number_of_zeros
 
 
 # ----------------------------------------------------------------------
